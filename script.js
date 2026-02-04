08e51f60a9418377228c925ff76a89fdf9ac2c2e
endothermiic <christina.pizzonia@mail.utoronto.ca>
2026-02-04 15:56:40 -0500

    added open command

--- script.js
+++ script.js
@@ -6,6 +6,57 @@ var srccode = "https://github.com/endothermiic/portfolio";
 var email = 'mailto:christina.pizzonia@mail.utoronto.ca';
 var emailnomail = 'christina [dot] pizzonia [at] mail [dot] utoronto [dot] ca';
 var linkedin = "https://www.linkedin.com/in/christina-pizzonia-05ab1a14a/";
+// Current working directory
+var cwd = "/home/cpizzonia"; // default location
+
+// Mini filesystem
+var fs = {
+    "/": { type: "dir", children: ["home"] },
+    "/home": { type: "dir", children: ["cpizzonia"] },
+    "/home/cpizzonia": { type: "dir", children: ["projects", "github", "linkedin", "resume", "src", "email"] },
+
+    // Projects directory
+    "/home/cpizzonia/projects": { type: "dir", children: ["space_invaders", "autonomous_robot", "sauna_redesign", "manhattan_plots"] },
+
+    // Project files
+    "/home/cpizzonia/projects/space_invaders": { type: "file", command: () => window.open("https://github.com/endothermiic/space_invaders", "_blank") },
+    "/home/cpizzonia/projects/autonomous_robot": { type: "file", command: () => window.open("https://github.com/endothermiic/autonomous_robot", "_blank") },
+    "/home/cpizzonia/projects/sauna_redesign": { type: "file", command: () => window.open("https://docs.google.com/presentation/d/1E1__1gkfL-J-T4yYeGk8QzZPa56Gy8qQq15TEvjyjhc/edit?usp=sharing", "_blank") },
+    "/home/cpizzonia/projects/manhattan_plots": { type: "file", command: () => window.open("https://my.locuszoom.org/gwas/482189/", "_blank") },
+
+    // Other links to open
+    "/home/cpizzonia/github": {
+        type: "file",
+        command: function (term) {
+            openLink(term, github, "github");
+        }
+    },
+    "/home/cpizzonia/linkedin": {
+        type: "file",
+        command: function (term) {
+            openLink(term, linkedin, "linkedin");
+        }
+    },
+    "/home/cpizzonia/resume": {
+        type: "file",
+        command: function (term) {
+            openLink(term, resume, "resume");
+        }
+    },
+    "/home/cpizzonia/email": {
+        type: "file",
+        command: function (term) {
+            openLink(term, email, "email");
+        }
+    }
+
+    // "/home/cpizzonia/github": { type: "file", command: () => openLink(github) },
+    // "/home/cpizzonia/linkedin": { type: "file", command: () => openLink(linkedin) },
+    // "/home/cpizzonia/resume": { type: "file", command: () => openLink(resume) },
+    // "/home/cpizzonia/src": { type: "file", command: () => openLink(srccode) },
+    // "/home/cpizzonia/email": { type: "file", command: () => window.open(email, "_blank") }
+};
+
 
 help = [
     'email          best place to contact me (warning: opens in new tab)\n' +
@@ -15,15 +66,17 @@ help = [
     'projects       some cool stuff I\'ve done\n' +
     'resume         display resume (warning: opens in new tab)\n' +
     'skills         languages and software I use often\n' +
-    'spotify        some of the music I enjoy (warning: opens in new tab)\n' +
     'src            source code for this site (warning: opens in new tab)\n' +
     'whoami         who is endothermiic, really?'
     // 'reflections         WEEKLY SEMI-RANDOM OBSERVATIONS',
     // 'hidden         IT\'S A SECRET',
 ]
 
-whoami = "Hi! I'm Christina, and I'm currently a 2nd year electrical engineering student at the University of Toronto.\n" +
-    "I'm interested in signals, communications and analog electronics.";
+var whoami = "Hello! I'm Christina. I'm currently a 4th electrical engineering student at " + '<a href="https://www.utoronto.ca/" target="_blank">UofT</a>.<br>' +
+    "I am someone who enjoys learning all kinds of things, which has lent itself to a diverse portfolio of projects and work experiences." +
+    " Most recently, I have worked as a digital design engineer at Cadence Design Systems, working on high-speed SerDes PCIe PHY, and served as Editor-in-Chief of " +
+    '<a href="https://cannon.skule.ca" target="_blank">The Cannon</a>, the official magazine of the Engineering Society.' +
+    ' I have taken courses in RTL design, computer architecture and digital signal processing, but am also interested in power systems. I am currently in the process of completing a capstone project centred on optimizing scheduling for EV charging in MATLAB.\n';
 //^insert rough timelinehere?
 
 projects = [ //also - menu for proejcts? ls type with each project getting its own subfolder?
@@ -45,7 +98,10 @@ $('body').terminal({
     //temporary function to test image insertion; going to update projects over the next few weeks
     mpv: function () {
         this.echo($('<img src="images/front.png" width="200" ></img>'));
-        this.echo($('<a href="https://www.utoronto.ca/">link text</a>'));
+        this.echo($('<a href="https://www.utoronto.ca/">UofT</a>'));
+    },
+    pwd: function () {
+        this.echo(cwd);
     },
     email: function () {
         this.echo("Successfully opened draft to email " + emailnomail);
@@ -59,6 +115,9 @@ $('body').terminal({
             'cancelable': true
         });
         linkElement.dispatchEvent(event);
+    },
+    whoami: function () {
+        this.echo(whoami, { raw: true }); // raw:true renders HTML
     },
     github: function () {
         openLink.call(this, github);
@@ -83,9 +142,49 @@ $('body').terminal({
     },
     src: function () {
         openLink.call(this, srccode);
+    },
+    open: function (file) {
+        if (!file) return this.error("open: missing file argument");
+        var path = cwd === "/" ? "/" + file : cwd + "/" + file;
+        if (fs[path] && fs[path].type === "file") {
+            fs[path].command(this); // <-- THIS passes the terminal
+        } else {
+            this.error("open: " + file + ": No such file");
+        }
+    },
+    ls: function () {
+        var dir = fs[cwd];
+        if (!dir || dir.type !== "dir") {
+            this.error("ls: cannot access '" + cwd + "': Not a directory");
+            return;
+        }
+        // list children with styling
+        var list = dir.children.map(child => {
+            var path = cwd === "/" ? "/" + child : cwd + "/" + child;
+            var type = fs[path]?.type;
+            if (type === "dir") return child + "/";
+            else return child; // executables
+        });
+        this.echo(list.join("  "));
+    },
+    cd: function (path) {
+        if (path === "..") {
+            if (cwd !== "/") {
+                cwd = cwd.split("/").slice(0, -1).join("/");
+                if (cwd === "") cwd = "/";
+            }
+            return;
+        }
+        var newPath = cwd === "/" ? "/" + path : cwd + "/" + path;
+        if (fs[newPath] && fs[newPath].type === "dir") {
+            cwd = newPath;
+        } else {
+            this.error("cd: " + path + ": No such directory");
+        }
     }
 }, {
-    prompt: 'cpizzonia@endothermiic ~ % ', greetings:
+    prompt: function () { return 'cpizzonia@portfolio:: ' + cwd + ' > '; },
+    greetings:
 
 
         '               _       _   _                         _ _\n' +
@@ -99,16 +198,9 @@ $('body').terminal({
 });
 
 
-function openLink(link) {
-    this.echo("The webpage " + link + " was successfully opened.");
-    var linkElement = $('<a>', {
-        href: link,
-        target: '_blank',
-    }).get(0);
-    var event = new MouseEvent('click', {
-        'view': window,
-        'bubbles': true,
-        'cancelable': true
-    });
+function openLink(term, link, label) {
+    term.echo("successfully opened " + label);
+    var linkElement = $('<a>', { href: link, target: '_blank' }).get(0);
+    var event = new MouseEvent('click', { view: window, bubbles: true, cancelable: true });
     linkElement.dispatchEvent(event);
 }
